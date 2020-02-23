f994af3087a70ec3c991adb45bef17905db03252
Inaki Segura <isegura@hawk.iit.edu>
2020-02-22 19:24:12 -0600

    Added 192 bit cipher versions. Added SM4 and SEED ciphers

--- src/open_ssl_cipher_factory.cpp
+++ src/open_ssl_cipher_factory.cpp
@@ -10,6 +10,11 @@
 
 #define CIPHER(key_len, block_len, cipher) (CipherPtr(new OpenSSLCipherImpl<key_len, block_len>(cipher)))
 
+#define KEY_128 16
+#define KEY_192 24
+#define KEY_256 32
+#define KEY_448 56
+
 template <int KEY_LENGTH>
 class OpenSSLCipher128Block : public OpenSSLCipher<KEY_LENGTH, 16>
 {
@@ -45,34 +50,58 @@ CipherPtr OpenSSLCipherFactory::getCipher(Cipher cipher)
     switch (cipher)
     {
         case Cipher::AES_256_CBC:
-            return CIPHER_128_BLOCK(32, EVP_aes_256_cbc());
+            return CIPHER_128_BLOCK(KEY_256, EVP_aes_256_cbc());
         case Cipher::AES_256_CFB:
-            return CIPHER_128_BLOCK(32, EVP_aes_256_cfb());
+            return CIPHER_128_BLOCK(KEY_256, EVP_aes_256_cfb());
         case Cipher::AES_256_ECB:
-            return CIPHER_128_BLOCK(32, EVP_aes_256_ecb());
+            return CIPHER_128_BLOCK(KEY_256, EVP_aes_256_ecb());
+        case Cipher::AES_192_CBC:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aes_192_cbc());
+        case Cipher::AES_192_CFB:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aes_192_cfb());
+        case Cipher::AES_192_ECB:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aes_192_ecb());
         case Cipher::AES_128_CBC:
-            return CIPHER_128_BLOCK(16, EVP_aes_128_cbc());
+            return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_cbc());
         case Cipher::AES_128_CFB:
-            return CIPHER_128_BLOCK(16, EVP_aes_128_cfb());
+            return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_cfb());
         case Cipher::AES_128_ECB:
-            return CIPHER_128_BLOCK(16, EVP_aes_128_ecb());
+            return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_ecb());
         case Cipher::ARIA_256_CBC:
-            return CIPHER_128_BLOCK(32, EVP_aria_256_cbc());
+            return CIPHER_128_BLOCK(KEY_256, EVP_aria_256_cbc());
         case Cipher::ARIA_256_CFB:
-            return CIPHER_128_BLOCK(32, EVP_aria_256_cfb());
+            return CIPHER_128_BLOCK(KEY_256, EVP_aria_256_cfb());
         case Cipher::ARIA_256_ECB:
-            return CIPHER_128_BLOCK(32, EVP_aria_256_ecb());
+            return CIPHER_128_BLOCK(KEY_256, EVP_aria_256_ecb());
+        case Cipher::ARIA_192_CBC:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aria_192_cbc());
+        case Cipher::ARIA_192_CFB:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aria_192_cfb());
+        case Cipher::ARIA_192_ECB:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aria_192_ecb());
         case Cipher::ARIA_128_CBC:
-            return CIPHER_128_BLOCK(16, EVP_aria_128_cbc());
+            return CIPHER_128_BLOCK(KEY_128, EVP_aria_128_cbc());
         case Cipher::ARIA_128_CFB:
-            return CIPHER_128_BLOCK(16, EVP_aria_128_cfb());
+            return CIPHER_128_BLOCK(KEY_128, EVP_aria_128_cfb());
         case Cipher::ARIA_128_ECB:
-            return CIPHER_128_BLOCK(16, EVP_aria_128_ecb());
+            return CIPHER_128_BLOCK(KEY_128, EVP_aria_128_ecb());
+        case Cipher::SM4_CBC:
+            return CIPHER_128_BLOCK(KEY_128, EVP_sm4_cbc());
+        case Cipher::SM4_CFB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_sm4_cfb());
+        case Cipher::SM4_ECB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_sm4_ecb());
+        case Cipher::SEED_CBC:
+            return CIPHER_128_BLOCK(KEY_128, EVP_seed_cbc());
+        case Cipher::SEED_CFB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_seed_cfb());
+        case Cipher::SEED_ECB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_seed_ecb());
         case Cipher::BLOWFISH_CBC:
-            return CIPHER(56, 8, EVP_bf_cbc());
+            return CIPHER(KEY_448, 8, EVP_bf_cbc());
         case Cipher::BLOWFISH_ECB:
-            return CIPHER(56, 8, EVP_bf_ecb());
+            return CIPHER(KEY_448, 8, EVP_bf_ecb());
         case Cipher::BLOWFISH_CFB:
-            return CIPHER(56, 8, EVP_bf_cfb());
+            return CIPHER(KEY_448, 8, EVP_bf_cfb());
     }
 }
