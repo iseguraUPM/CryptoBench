b3ca031e0a1703fa1ade6fba33018d00f0f696bc
Inaki Segura <isegura@hawk.iit.edu>
2020-03-24 11:05:48 -0500

    Added more openssl block modes

--- include/CryptoBench/cipher_factory.hpp
+++ include/CryptoBench/cipher_factory.hpp
@@ -18,6 +18,10 @@
 #define STR_ECB "ECB"
 #define STR_CFB "CFB"
 #define STR_GCM "GCM"
+#define STR_CTR "CTR"
+#define STR_OFB "OFB"
+#define STR_OCB "OCB"
+#define STR_XTS "XTS"
 
 using CipherPtr = std::shared_ptr<SymmetricCipher>;
 
@@ -26,30 +30,60 @@ enum class Cipher
     AES_256_CBC,
     AES_256_CFB,
     AES_256_ECB,
+    AES_256_CTR,
+    AES_256_OCB,
+    AES_256_OFB,
+    AES_256_XTS,
+
     AES_192_CBC,
     AES_192_CFB,
     AES_192_ECB,
+    AES_192_CTR,
+    AES_192_OFB,
+    AES_192_OCB,
+
     AES_128_CBC,
     AES_128_CFB,
     AES_128_ECB,
+    AES_128_CTR,
+    AES_128_OFB,
+    AES_128_OCB,
+    AES_128_XTS,
+
     ARIA_256_CBC,
     ARIA_256_CFB,
     ARIA_256_ECB,
+    ARIA_256_OFB,
+    ARIA_256_CTR,
+
     ARIA_192_CBC,
     ARIA_192_CFB,
     ARIA_192_ECB,
+    ARIA_192_OFB,
+    ARIA_192_CTR,
+
     ARIA_128_CBC,
     ARIA_128_CFB,
     ARIA_128_ECB,
+    ARIA_128_OFB,
+    ARIA_128_CTR,
+
     SM4_CBC,
     SM4_CFB,
     SM4_ECB,
+    SM4_CTR,
+    SM4_OFB,
+
     SEED_CBC,
     SEED_CFB,
     SEED_ECB,
+    SEED_OFB,
+
     BLOWFISH_CBC,
     BLOWFISH_ECB,
     BLOWFISH_CFB,
+    BLOWFISH_OFB,
+
     AES_256_GCM
 };
 
@@ -93,54 +127,96 @@ inline std::pair<std::string, std::string> cipherDescription(Cipher cipher)
             return std::make_pair(STR_AES, STR_CFB);
         case Cipher::AES_256_ECB:
             return std::make_pair(STR_AES, STR_ECB);
+        case Cipher::AES_256_CTR:
+            return std::make_pair(STR_AES, STR_CTR);
+        case Cipher::AES_256_OFB:
+            return std::make_pair(STR_AES, STR_OFB);
+        case Cipher::AES_256_OCB:
+            return std::make_pair(STR_AES, STR_OCB);
+        case Cipher::AES_256_XTS:
+            return std::make_pair(STR_AES, STR_XTS);
         case Cipher::AES_192_CBC:
             return std::make_pair(STR_AES, STR_CBC);
         case Cipher::AES_192_CFB:
             return std::make_pair(STR_AES, STR_CFB);
         case Cipher::AES_192_ECB:
             return std::make_pair(STR_AES, STR_ECB);
+        case Cipher::AES_192_CTR:
+            return std::make_pair(STR_AES, STR_CTR);
+        case Cipher::AES_192_OCB:
+            return std::make_pair(STR_AES, STR_OCB);
+        case Cipher::AES_192_OFB:
+            return std::make_pair(STR_AES, STR_OFB);
         case Cipher::AES_128_CBC:
             return std::make_pair(STR_AES, STR_CBC);
         case Cipher::AES_128_CFB:
             return std::make_pair(STR_AES, STR_CFB);
         case Cipher::AES_128_ECB:
             return std::make_pair(STR_AES, STR_ECB);
+        case Cipher::AES_128_CTR:
+            return std::make_pair(STR_AES, STR_CTR);
+        case Cipher::AES_128_OFB:
+            return std::make_pair(STR_AES, STR_OFB);
+        case Cipher::AES_128_OCB:
+            return std::make_pair(STR_AES, STR_OCB);
+        case Cipher::AES_128_XTS:
+            return std::make_pair(STR_AES, STR_XTS);
         case Cipher::ARIA_256_CBC:
             return std::make_pair(STR_ARIA, STR_CBC);
         case Cipher::ARIA_256_CFB:
             return std::make_pair(STR_ARIA, STR_CFB);
         case Cipher::ARIA_256_ECB:
             return std::make_pair(STR_ARIA, STR_ECB);
+        case Cipher::ARIA_256_CTR:
+            return std::make_pair(STR_ARIA, STR_CTR);
+        case Cipher::ARIA_256_OFB:
+            return std::make_pair(STR_ARIA, STR_OFB);
         case Cipher::ARIA_192_CBC:
             return std::make_pair(STR_ARIA, STR_CBC);
         case Cipher::ARIA_192_CFB:
             return std::make_pair(STR_ARIA, STR_CFB);
         case Cipher::ARIA_192_ECB:
             return std::make_pair(STR_ARIA, STR_ECB);
+        case Cipher::ARIA_192_CTR:
+            return std::make_pair(STR_ARIA, STR_CTR);
+        case Cipher::ARIA_192_OFB:
+            return std::make_pair(STR_ARIA, STR_OFB);
         case Cipher::ARIA_128_CBC:
             return std::make_pair(STR_ARIA, STR_CBC);
         case Cipher::ARIA_128_CFB:
             return std::make_pair(STR_ARIA, STR_CFB);
         case Cipher::ARIA_128_ECB:
             return std::make_pair(STR_ARIA, STR_ECB);
+        case Cipher::ARIA_128_CTR:
+            return std::make_pair(STR_ARIA, STR_CTR);
+        case Cipher::ARIA_128_OFB:
+            return std::make_pair(STR_ARIA, STR_OFB);
         case Cipher::SM4_CBC:
             return std::make_pair(STR_SM4, STR_CBC);
         case Cipher::SM4_CFB:
             return std::make_pair(STR_SM4, STR_CFB);
         case Cipher::SM4_ECB:
             return std::make_pair(STR_SM4, STR_ECB);
+        case Cipher::SM4_CTR:
+            return std::make_pair(STR_SM4, STR_CTR);
+        case Cipher::SM4_OFB:
+            return std::make_pair(STR_SM4, STR_OFB);
         case Cipher::SEED_CBC:
             return std::make_pair(STR_SEED, STR_CBC);
         case Cipher::SEED_CFB:
             return std::make_pair(STR_SEED, STR_CFB);
         case Cipher::SEED_ECB:
             return std::make_pair(STR_SEED, STR_ECB);
+        case Cipher::SEED_OFB:
+            return std::make_pair(STR_SEED, STR_OFB);
         case Cipher::BLOWFISH_CBC:
             return std::make_pair(STR_BLOWFISH, STR_CBC);
         case Cipher::BLOWFISH_ECB:
             return std::make_pair(STR_BLOWFISH, STR_ECB);
         case Cipher::BLOWFISH_CFB:
             return std::make_pair(STR_BLOWFISH, STR_CFB);
+        case Cipher::BLOWFISH_OFB:
+            return std::make_pair(STR_BLOWFISH, STR_OFB);
         case Cipher::AES_256_GCM:
             return std::make_pair(STR_AES, STR_GCM);
     }
--- src/open_ssl_cipher_factory.cpp
+++ src/open_ssl_cipher_factory.cpp
@@ -13,6 +13,7 @@
 #define KEY_128 16
 #define KEY_192 24
 #define KEY_256 32
+#define KEY_512 64
 #define KEY_448 56
 
 template <int KEY_LENGTH>
@@ -55,54 +56,96 @@ CipherPtr OpenSSLCipherFactory::getCipher(Cipher cipher)
             return CIPHER_128_BLOCK(KEY_256, EVP_aes_256_cfb());
         case Cipher::AES_256_ECB:
             return CIPHER_128_BLOCK(KEY_256, EVP_aes_256_ecb());
+        case Cipher::AES_256_CTR:
+            return CIPHER_128_BLOCK(KEY_256, EVP_aes_256_ctr());
+        case Cipher::AES_256_OFB:
+            return CIPHER_128_BLOCK(KEY_256, EVP_aes_256_ofb());
+        case Cipher::AES_256_OCB:
+            return CIPHER_128_BLOCK(KEY_256, EVP_aes_256_ocb());
+        case Cipher::AES_256_XTS:
+            return CIPHER_128_BLOCK(KEY_512, EVP_aes_256_xts()); // XTS mode expects key doubled
         case Cipher::AES_192_CBC:
             return CIPHER_128_BLOCK(KEY_192, EVP_aes_192_cbc());
         case Cipher::AES_192_CFB:
             return CIPHER_128_BLOCK(KEY_192, EVP_aes_192_cfb());
         case Cipher::AES_192_ECB:
             return CIPHER_128_BLOCK(KEY_192, EVP_aes_192_ecb());
+        case Cipher::AES_192_CTR:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aes_192_ctr());
+        case Cipher::AES_192_OFB:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aes_192_ofb());
+        case Cipher::AES_192_OCB:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aes_256_ocb());
         case Cipher::AES_128_CBC:
             return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_cbc());
         case Cipher::AES_128_CFB:
             return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_cfb());
         case Cipher::AES_128_ECB:
             return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_ecb());
+        case Cipher::AES_128_CTR:
+            return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_ctr());
+        case Cipher::AES_128_OFB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_ofb());
+        case Cipher::AES_128_OCB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_aes_128_ocb());
+        case Cipher::AES_128_XTS:
+            return CIPHER_128_BLOCK(KEY_256, EVP_aes_128_xts()); // XTS mode expects key doubled
         case Cipher::ARIA_256_CBC:
             return CIPHER_128_BLOCK(KEY_256, EVP_aria_256_cbc());
         case Cipher::ARIA_256_CFB:
             return CIPHER_128_BLOCK(KEY_256, EVP_aria_256_cfb());
         case Cipher::ARIA_256_ECB:
             return CIPHER_128_BLOCK(KEY_256, EVP_aria_256_ecb());
+        case Cipher::ARIA_256_CTR:
+            return CIPHER_128_BLOCK(KEY_256, EVP_aria_256_ctr());
+        case Cipher::ARIA_256_OFB:
+            return CIPHER_128_BLOCK(KEY_256, EVP_aria_256_ofb());
         case Cipher::ARIA_192_CBC:
             return CIPHER_128_BLOCK(KEY_192, EVP_aria_192_cbc());
         case Cipher::ARIA_192_CFB:
             return CIPHER_128_BLOCK(KEY_192, EVP_aria_192_cfb());
         case Cipher::ARIA_192_ECB:
             return CIPHER_128_BLOCK(KEY_192, EVP_aria_192_ecb());
+        case Cipher::ARIA_192_CTR:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aria_192_ctr());
+        case Cipher::ARIA_192_OFB:
+            return CIPHER_128_BLOCK(KEY_192, EVP_aria_192_ofb());
         case Cipher::ARIA_128_CBC:
             return CIPHER_128_BLOCK(KEY_128, EVP_aria_128_cbc());
         case Cipher::ARIA_128_CFB:
             return CIPHER_128_BLOCK(KEY_128, EVP_aria_128_cfb());
         case Cipher::ARIA_128_ECB:
             return CIPHER_128_BLOCK(KEY_128, EVP_aria_128_ecb());
+        case Cipher::ARIA_128_CTR:
+            return CIPHER_128_BLOCK(KEY_128, EVP_aria_128_ctr());
+        case Cipher::ARIA_128_OFB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_aria_128_ofb());
         case Cipher::SM4_CBC:
             return CIPHER_128_BLOCK(KEY_128, EVP_sm4_cbc());
         case Cipher::SM4_CFB:
             return CIPHER_128_BLOCK(KEY_128, EVP_sm4_cfb());
         case Cipher::SM4_ECB:
             return CIPHER_128_BLOCK(KEY_128, EVP_sm4_ecb());
+        case Cipher::SM4_CTR:
+            return CIPHER_128_BLOCK(KEY_128, EVP_sm4_ctr());
+        case Cipher::SM4_OFB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_sm4_ofb());
         case Cipher::SEED_CBC:
             return CIPHER_128_BLOCK(KEY_128, EVP_seed_cbc());
         case Cipher::SEED_CFB:
             return CIPHER_128_BLOCK(KEY_128, EVP_seed_cfb());
         case Cipher::SEED_ECB:
             return CIPHER_128_BLOCK(KEY_128, EVP_seed_ecb());
+        case Cipher::SEED_OFB:
+            return CIPHER_128_BLOCK(KEY_128, EVP_seed_ofb());
         case Cipher::BLOWFISH_CBC:
             return CIPHER(KEY_448, 8, EVP_bf_cbc());
         case Cipher::BLOWFISH_ECB:
             return CIPHER(KEY_448, 8, EVP_bf_ecb());
         case Cipher::BLOWFISH_CFB:
             return CIPHER(KEY_448, 8, EVP_bf_cfb());
+        case Cipher::BLOWFISH_OFB:
+            return CIPHER(KEY_448, 8, EVP_bf_ofb());
         default:
             return nullptr;
     }
