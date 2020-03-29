2092e3e7ff3a4dbb246379fd9140616f2051ae09
Inaki Segura <isegura@hawk.iit.edu>
2020-03-29 12:26:37 -0500

    64 bit size_t specification

--- include/CryptoBench/symmetric_cipher.hpp
+++ include/CryptoBench/symmetric_cipher.hpp
@@ -12,14 +12,16 @@
 
 typedef unsigned char byte;
 
+typedef unsigned long long int byte_len;
+
 class SymmetricCipher
 {
 public:
 
-    virtual void encrypt(const byte* key, const byte* plain_text, size_t plain_text_len
-            , byte* cipher_text, size_t cipher_text_len) = 0;
-    virtual void decrypt(const byte* key, const byte* cipher_text, size_t cipher_text_len
-            , byte* recovered_text, size_t recovered_text_len) = 0;
+    virtual void encrypt(const byte* key, const byte * plain_text, byte_len plain_text_len
+            , byte * cipher_text, byte_len & cipher_text_len) = 0;
+    virtual void decrypt(const byte* key, const byte * cipher_text, byte_len cipher_text_len
+            , byte * recovered_text, byte_len & recovered_text_len) = 0;
 
     virtual int getBlockLen() = 0;
 
