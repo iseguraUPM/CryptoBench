87d8a37777839fcf1373e3792bc07fbc6fd6da0e
Juan Pablo <juanpamz@gmail.com>
2020-03-31 17:16:10 -0500

    Fixed botan OCB block size

--- src/botan_cipher_factory.cpp
+++ src/botan_cipher_factory.cpp
@@ -2,6 +2,7 @@
 // Created by Juan Pablo Melgarejo on 3/30/20.
 //
 
+#include <CryptoBench/cipher_exception.hpp>
 #include "CryptoBench/botan_cipher_factory.hpp"
 
 
@@ -24,12 +25,11 @@ CipherPtr BotanCipherFactory::getCipher(Cipher cipher)
         case Cipher::AES_256_CFB:
             return BOTAN_CIPHER(KEY_256, 16, "AES-256/CFB");
         case Cipher::AES_256_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_256, 16, "AES-256/ECB");
+            throw UnsupportedCipherException();
         case Cipher::AES_256_CTR:
             return BOTAN_CIPHER(KEY_256, 16, "AES-256/CTR");
         case Cipher::AES_256_OCB:
-            return BOTAN_CIPHER(KEY_256, 16, "AES-256/OCB");
+            return BOTAN_CIPHER(KEY_256, 12, "AES-256/OCB");
         case Cipher::AES_256_OFB:
             return BOTAN_CIPHER(KEY_256, 16, "AES-256/OFB");
         case Cipher::AES_256_XTS:
@@ -41,14 +41,13 @@ CipherPtr BotanCipherFactory::getCipher(Cipher cipher)
         case Cipher::AES_192_CFB:
             return BOTAN_CIPHER(KEY_192, 16, "AES-192/CFB");
         case Cipher::AES_192_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_192, 16, "AES-192/ECB");
+            throw UnsupportedCipherException();
         case Cipher::AES_192_CTR:
             return BOTAN_CIPHER(KEY_192, 16, "AES-192/CTR");
         case Cipher::AES_192_OFB:
             return BOTAN_CIPHER(KEY_192, 16, "AES-192/OFB");
         case Cipher::AES_192_OCB:
-            return BOTAN_CIPHER(KEY_192, 16, "AES-192/OCB");
+            return BOTAN_CIPHER(KEY_192, 12, "AES-192/OCB");
         case Cipher::AES_192_GCM:
             return BOTAN_CIPHER(KEY_192, 16, "AES-192/GCM");
         case Cipher::AES_128_CBC:
@@ -56,14 +55,13 @@ CipherPtr BotanCipherFactory::getCipher(Cipher cipher)
         case Cipher::AES_128_CFB:
             return BOTAN_CIPHER(KEY_128, 16, "AES-128/CFB");
         case Cipher::AES_128_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_128, 16, "AES-128/ECB");
+            throw UnsupportedCipherException();
         case Cipher::AES_128_CTR:
             return BOTAN_CIPHER(KEY_128, 16, "AES-128/CTR");
         case Cipher::AES_128_OFB:
             return BOTAN_CIPHER(KEY_128, 16, "AES-128/OFB");
         case Cipher::AES_128_OCB:
-            return BOTAN_CIPHER(KEY_128, 16, "AES-128/OCB");
+            return BOTAN_CIPHER(KEY_128, 12, "AES-128/OCB");
         case Cipher::AES_128_XTS:
             return BOTAN_CIPHER(KEY_128, 16, "AES-128/XTS");
         case Cipher::AES_128_GCM:
@@ -73,8 +71,7 @@ CipherPtr BotanCipherFactory::getCipher(Cipher cipher)
         case Cipher::ARIA_256_CFB:
             return BOTAN_CIPHER(KEY_256, 16, "ARIA-256/CFB");
         case Cipher::ARIA_256_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_256, 16,"ARIA-256/ECB");
+            throw UnsupportedCipherException();
         case Cipher::ARIA_256_OFB:
             return BOTAN_CIPHER(KEY_256, 16, "ARIA-256/OFB");
         case Cipher::ARIA_256_CTR:
@@ -86,8 +83,7 @@ CipherPtr BotanCipherFactory::getCipher(Cipher cipher)
         case Cipher::ARIA_192_CFB:
             return BOTAN_CIPHER(KEY_192, 16, "ARIA-192/CFB");
         case Cipher::ARIA_192_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_192, 16, "ARIA-192/ECB");
+            throw UnsupportedCipherException();
         case Cipher::ARIA_192_OFB:
             return BOTAN_CIPHER(KEY_192, 16, "ARIA-192/OFB");
         case Cipher::ARIA_192_CTR:
@@ -99,43 +95,39 @@ CipherPtr BotanCipherFactory::getCipher(Cipher cipher)
         case Cipher::ARIA_128_CFB:
             return BOTAN_CIPHER(KEY_128, 16, "ARIA-128/CFB");
         case Cipher::ARIA_128_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_128, 16, "ARIA-128/ECB");
+            throw UnsupportedCipherException();
         case Cipher::ARIA_128_OFB:
             return BOTAN_CIPHER(KEY_128, 16, "ARIA-128/OFB");
         case Cipher::ARIA_128_CTR:
             return BOTAN_CIPHER(KEY_128, 16, "ARIA-128/CTR");
         case Cipher::ARIA_128_GCM:
             return BOTAN_CIPHER(KEY_128, 16, "ARIA-128/GCM");
-        /*case Cipher::SM4_CBC:
-            return BOTAN_CIPHER(KEY_128, 16, "SM4-128/CBC");
+        case Cipher::SM4_CBC:
+            return BOTAN_CIPHER(KEY_128, 16, "SM4/CBC");
         case Cipher::SM4_CFB:
-            return BOTAN_CIPHER(KEY_128, 16, "SM4-128/CFB");
+            return BOTAN_CIPHER(KEY_128, 16, "SM4/CFB");
         case Cipher::SM4_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_128, 16, "SM4-128/ECB");
+            throw UnsupportedCipherException();
         case Cipher::SM4_CTR:
-            return BOTAN_CIPHER(KEY_128, 16, "SM4-128/CTR");
+            return BOTAN_CIPHER(KEY_128, 16, "SM4/CTR");
         case Cipher::SM4_OFB:
-            return BOTAN_CIPHER(KEY_128, 16, "SM4-128/OFB");
+            return BOTAN_CIPHER(KEY_128, 16, "SM4/OFB");
         case Cipher::SEED_CBC:
-            return BOTAN_CIPHER(KEY_128, 16, "SEED-128/CBC");
+            return BOTAN_CIPHER(KEY_128, 16, "SEED/CBC");
         case Cipher::SEED_CFB:
-            return BOTAN_CIPHER(KEY_128, 16, "SEED-128/CFB");
+            return BOTAN_CIPHER(KEY_128, 16, "SEED/CFB");
         case Cipher::SEED_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_128, 16, "SEED-128/ECB");
+            throw UnsupportedCipherException();
         case Cipher::SEED_OFB:
-            return BOTAN_CIPHER(KEY_128, 16, "SEED-128/OFB");
+            return BOTAN_CIPHER(KEY_128, 16, "SEED/OFB");
         case Cipher::BLOWFISH_CBC:
-            return BOTAN_CIPHER(KEY_448, 8, "BLOWFISH-448/CBC");
+            return BOTAN_CIPHER(KEY_448, 8, "Blowfish/CBC");
         case Cipher::BLOWFISH_ECB:
-            return nullptr;
-            return BOTAN_CIPHER(KEY_448, 8, "BLOWFISH-448/ECB");
+            throw UnsupportedCipherException();
         case Cipher::BLOWFISH_CFB:
-            return BOTAN_CIPHER(KEY_448, 8, "BLOWFISH-448/CFB");
+            return BOTAN_CIPHER(KEY_448, 8, "Blowfish/CFB");
         case Cipher::BLOWFISH_OFB:
-            return BOTAN_CIPHER(KEY_448, 8, "BLOWFISH-448/OFB");*/
+            return BOTAN_CIPHER(KEY_448, 8, "Blowfish/OFB");
         default:
             return nullptr;
     }
