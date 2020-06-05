f53ba349d77e9dc189b360162318e686067db1b4
Juan Pablo <juanpamz@gmail.com>
2020-06-05 10:49:21 -0500

    Placed code inside a function

--- CipherTreeTool/fill_security_filebytes.py
+++ CipherTreeTool/fill_security_filebytes.py
@@ -1,19 +1,20 @@
 import pandas as pd
 
-df = pd.read_csv('dummy_data.csv')
+mydf = pd.read_csv('dummy_data.csv')
 
-sec_levels = df['SEC_LEVEL'].unique()
 
-min_size = df['FILE_BYTES'].min()
-max_size = df['FILE_BYTES'].max()
+def fill_security_levels(df):
+    sec_levels = df['SEC_LEVEL'].unique()
+    max_size = df['FILE_BYTES'].max()
 
-for sec_level in sec_levels:
-    file_bytes = 1
-    print('sec_level-', sec_level)
-    while file_bytes < max_size + 1:
-        print('file_bytes-', file_bytes)
-        if df[(df['SEC_LEVEL'] == sec_level) & (df['FILE_BYTES'] == file_bytes)].shape[0] == 0:
-            df = df.append(((df[(df['SEC_LEVEL'] == sec_level) & (df['FILE_BYTES'] == file_bytes/2)]).replace({'FILE_BYTES': file_bytes/2}, file_bytes)), ignore_index=True)
-        file_bytes = file_bytes * 2
+    for sec_level in sec_levels:
+        file_bytes = 1
+        while file_bytes < max_size + 1:
+            if df[(df['SEC_LEVEL'] == sec_level) & (df['FILE_BYTES'] == file_bytes)].shape[0] == 0:
+                df = df.append(((df[(df['SEC_LEVEL'] == sec_level) & (df['FILE_BYTES'] == file_bytes/2)]).replace({'FILE_BYTES': file_bytes/2}, file_bytes)), ignore_index=True)
+            file_bytes = file_bytes * 2
 
-df.to_csv('dummy_filled.csv', index=False)
+
+fill_security_levels(mydf)
+
+mydf.to_csv('dummy_filled.csv', index=False)
