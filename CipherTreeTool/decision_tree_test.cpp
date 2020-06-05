c184b4c0501de0828a11436549fc731fed95ec70
Inaki Segura <isegura@hawk.iit.edu>
2020-06-05 12:36:26 -0500

    Decision tree speed test

--- CipherTreeTool/decision_tree_test.cpp
+++ CipherTreeTool/decision_tree_test.cpp
@@ -3,10 +3,17 @@
 //
 
 #include <iostream>
+#include <chrono>
 
 #include "cipher_decision_tree.h"
 
 int main(int argc, char** argv)
 {
-    printTree();
+    //printTree();
+    CipherInfo info;
+    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::steady_clock::now();
+    findCipher(800, 2, info);
+    std::chrono::steady_clock::steady_clock::time_point t2 = std::chrono::steady_clock::steady_clock::now();
+    long encrypt_time_nano = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+    std::cout << "t: " << encrypt_time_nano << " Choice: " << info.lib << "-" << info.alg << "-" << info.key_bits << "-" << info.mode << "\n";
 }
