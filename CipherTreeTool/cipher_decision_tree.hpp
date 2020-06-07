afa1921b77c77acf6726aa899f0d8a4c46a18816
Inaki Segura <isegura@hawk.iit.edu>
2020-06-07 13:16:58 -0500

    Added test for cryptoengine

--- CipherTreeTool/cipher_decision_tree.hpp
+++ CipherTreeTool/cipher_decision_tree.hpp
@@ -3,26 +3,31 @@
 
 #include <string>
 
-struct CipherInfo
+namespace cryptoengine
 {
-    std::string lib;
-    std::string alg;
-    int key_bits;
-    std::string mode;
-};
 
-/**
- * Print the decision tree in hierarchical format from the standard output
- */
-void printTree();
+    struct CipherInfo
+    {
+        std::string lib;
+        std::string alg;
+        int key_bits;
+        std::string mode;
+    };
 
-/**
- * Find the appropriate cipher given the requirements
- * @param file_size of the input file to encrypt
- * @param sec_level required for the encryption
- * @param cipher response struct
- * @return 1 if the cipher was found or 0 if there is no candidate
- */
-int findCipher(int file_size, int sec_level, struct CipherInfo &cipher);
+    /**
+     * Print the decision tree in hierarchical format from the standard output
+     */
+    void printTree();
+
+    /**
+     * Find the appropriate cipher given the requirements
+     * @param file_size of the input file to encrypt
+     * @param sec_level required for the encryption
+     * @param cipher response struct
+     * @return 1 if the cipher was found or 0 if there is no candidate
+     */
+    int findCipher(int file_size, int sec_level, struct CipherInfo &cipher);
+
+}
 
 #endif /*CRYPTOBENCH_CIPHER_DECISION_TREE_HPP*/
