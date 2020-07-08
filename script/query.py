ede78306bcc6c8dc4a9e6fbb5ce076373e02bb43
Inaki Segura <isegura@hawk.iit.edu>
2020-07-08 12:06:02 +0200

    Refactored script. Output to file. Filtered best percentile to reduce dataset

--- script/query.py
+++ script/query.py
@@ -1,4 +1,5 @@
 import sys
+import math
 import pandas as pd
 from queue import Queue
 
@@ -8,41 +9,60 @@ df = pd.read_csv('benchmark_x86_64.csv')
 modes_df = pd.read_csv('block_cipher_modes_w.csv')
 rounds_df = pd.read_csv('block_cipher_rounds.csv')
 
-df = add_sec_level(df, modes_df, rounds_df, 5)
+# Add security level
+df = add_sec_level(df, modes_df, rounds_df, 4)
 
-print('Blocks: ')
-blocks = df['FILE_BYTES'].sort_values().unique()
-print('{' + ','.join(map(str, blocks)) + '}')
-print("Length: " + str(len(blocks)))
-
+# Compute pace
 df['PACE'] = df['ENCRYPT_T'] / df['FILE_BYTES']
 df['PACE'] = df['PACE'] + df['DECRYPT_T'] / df['FILE_BYTES']
 
+# Average pace repetitions
+cols = ['LIB', 'ALG', 'KEY_LEN', 'BLOCK_MODE', 'SEC_LEVEL', 'FILE_BYTES']
+df = df.groupby(cols, as_index=False).mean()
+
+# Percentile of pace per file size. Higher is worse
+df['RANK'] = df.groupby(['SEC_LEVEL', 'FILE_BYTES'])['PACE'].rank(method='min', ascending=True, pct=True)
+
+# Filter 25% best in pace per file size
+df = df[df['RANK'] <= 0.25].reset_index()
+
+# Recover new list of ciphers
 cols = ['LIB', 'ALG', 'KEY_LEN', 'BLOCK_MODE', 'SEC_LEVEL']
 ciphers = df.groupby(cols).size().reset_index()
 
+with open('query.txt', 'w') as f:
+    # Print list of file sizes in order
+    f.write('Blocks: \n')
+    blocks = df['FILE_BYTES'].sort_values().unique()
+    f.write('{%s }\n' % ', '.join(map(str, blocks)))
+    f.write("Length: %s\n\n" % str(len(blocks)))
 
-print('Sec: ')
-sec_levels = ciphers['SEC_LEVEL']
-print('{' + ','.join(map(str, sec_levels)) + '}')
+    # Print cipher names
+    f.write('Ciphers: \n')
+    names = ciphers.apply(lambda row: '\"' + row['LIB'] + '-' + row['ALG'] + '-' + str(row['KEY_LEN']) + '-' + row['BLOCK_MODE'] + '\"', axis=1)
+    f.write('{%s }\n' % ', '.join(names))
+    f.write("Length: %s\n\n" % str(len(ciphers)))
 
-def getPace(row, size):
-    lib = row['LIB']
-    alg = row['ALG']
-    key = row['KEY_LEN']
-    blockm = row['BLOCK_MODE']
-    repetitions = df[(df['LIB'] == lib) & (df['ALG'] == alg) & (df['KEY_LEN'] == key) & (df['BLOCK_MODE'] == blockm) & (df['FILE_BYTES'] == size)]
-    if (repetitions.shape[0] == 0):
-        return '0'
-    pace = repetitions['PACE'].mean()
-    return str(int(pace * 1000000.0))
+    # Print security level in cipher order
+    f.write('Sec: \n')
+    sec_levels = ciphers['SEC_LEVEL']
+    f.write('{%s }\n' % ', '.join(map(str, sec_levels)))
+    f.write("Length: %s\n\n" % str(len(ciphers)))
 
+    # Print pace per block size in cipher order
+    pace_per_block = []
+    for index, row in ciphers.iterrows():
+        paces = []
+        for size in blocks:
+            pace = df[(df['LIB'] == row['LIB']) & (df['ALG'] == row['ALG']) & (df['KEY_LEN'] == row['KEY_LEN']) & (df['BLOCK_MODE'] == row['BLOCK_MODE']) & (df['FILE_BYTES'] == size)]['PACE']
+            if len(pace) == 0:
+                paces.append(0)
+            else:
+                paces.append(int(pace.iloc[0] * 1000000.0))
+        pace_per_block.append(paces)
 
-pace_str = ''
-for index, row in ciphers.iterrows():
-    paces = []
-    for size in blocks:
-        paces.append(getPace(row, size))
-    pace_str += '{' + ','.join(paces) + '},\n'
-
-print(pace_str)
+    f.write('Paces: \n{\n')
+    for paces in pace_per_block[:-1]:
+        f.write('{%s }, \n' % ', '.join(map(str, paces)))
+    f.write('{%s }\n' % ', '.join(map(str, pace_per_block[-1])))
+    f.write('}\n')
