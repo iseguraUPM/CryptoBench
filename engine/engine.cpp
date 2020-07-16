43ab0ad2a634e3304f32b2725985115a90c731f0
Inaki Segura <isegura@hawk.iit.edu>
2020-07-16 10:06:46 +0200

    Removed unused stuff

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -84,24 +84,22 @@ std::vector<EncryptTask> Engine::minimizeTime(int64_t file_size, int sec_level)
             all_tasks[proc_id][block_id] = std::vector<Task>(devices.size());
             for (int device_id = 0; device_id < devices.size(); device_id++)
             {
-                std::stringstream ss;
-                ss << "_" << block_id << "_" << proc_id << "_" << device_id;
-                sat::BoolVar chosen = cp_model.NewBoolVar().WithName("chosen" + ss.str());
+                sat::BoolVar chosen = cp_model.NewBoolVar();
                 all_chosen.push_back(chosen);
                 all_block_sizes.push_back(blocks[block_id]);
                 all_sec_levels.push_back(sec_levels[proc_id]);
 
                 sat::IntVar p_time = cp_model.NewConstant(blocks[block_id] * processors[proc_id][block_id]);
-                sat::IntVar p_start = cp_model.NewIntVar(domain).WithName("p_start" + ss.str());
-                sat::IntVar p_end = cp_model.NewIntVar(domain).WithName("p_end" + ss.str());
-                sat::IntervalVar p_interval = cp_model.NewOptionalIntervalVar(p_start, p_time, p_end, chosen).WithName("p_interval" + ss.str());
+                sat::IntVar p_start = cp_model.NewIntVar(domain);
+                sat::IntVar p_end = cp_model.NewIntVar(domain);
+                sat::IntervalVar p_interval = cp_model.NewOptionalIntervalVar(p_start, p_time, p_end, chosen);
 
                 all_p_intervals.push_back(p_interval);
 
                 sat::IntVar io_time = cp_model.NewConstant(blocks[block_id] * devices[device_id]);
-                sat::IntVar io_start = cp_model.NewIntVar(domain).WithName("io_start" + ss.str());
-                sat::IntVar io_end = cp_model.NewIntVar(domain).WithName("io_end" + ss.str());
-                sat::IntervalVar io_interval = cp_model.NewOptionalIntervalVar(io_start, io_time, io_end, chosen).WithName("io_interval" + ss.str());
+                sat::IntVar io_start = cp_model.NewIntVar(domain);
+                sat::IntVar io_end = cp_model.NewIntVar(domain);
+                sat::IntervalVar io_interval = cp_model.NewOptionalIntervalVar(io_start, io_time, io_end, chosen);
 
                 all_io_ends.push_back(io_end);
                 per_device_intervals[device_id].push_back(io_interval);
@@ -115,9 +113,6 @@ std::vector<EncryptTask> Engine::minimizeTime(int64_t file_size, int sec_level)
         }
     }
 
-    std::cout << "Finished preparing data" << std::endl;
-    std::cout << "\tConsidering " << task_count << " tasks" << std::endl;
-
     /// Constraints
     cp_model.AddNoOverlap(all_p_intervals);
     for (int device_id = 0; device_id < devices.size(); device_id++)
@@ -127,16 +122,12 @@ std::vector<EncryptTask> Engine::minimizeTime(int64_t file_size, int sec_level)
 
     cp_model.AddEquality(sat::LinearExpr::BooleanScalProd(all_chosen, all_block_sizes), file_size);
 
-    // Just for evaluation purposes
-    sat::IntVar overall_security = cp_model.NewIntVar(domain).WithName("overall_security");
-    cp_model.AddEquality(sat::LinearExpr::BooleanScalProd(all_chosen, all_sec_levels), overall_security);
-
     /// Objective
-    sat::IntVar obj_var = cp_model.NewIntVar(domain).WithName("makespan");
+    sat::IntVar obj_var = cp_model.NewIntVar(domain);
     cp_model.AddMaxEquality(obj_var, all_io_ends);
     cp_model.Minimize(obj_var);
 
-    sat::IntVar choices = cp_model.NewIntVar({0, task_count}).WithName("choices");
+    sat::IntVar choices = cp_model.NewIntVar({0, task_count});
     cp_model.AddEquality(choices, sat::LinearExpr::BooleanSum(all_chosen));
     cp_model.Minimize(choices);
 
@@ -154,9 +145,6 @@ std::vector<EncryptTask> Engine::minimizeTime(int64_t file_size, int sec_level)
     std::vector<EncryptTask> result;
     if (response.status() == sat::CpSolverStatus::OPTIMAL || response.status() == sat::CpSolverStatus::FEASIBLE)
     {
-        auto mean_sec_level = (double) sat::SolutionIntegerValue(response, overall_security) / sat::SolutionIntegerValue(response, choices);
-
-        std::stringstream processor_tasks;
         for (int proc_id = 0; proc_id < processors.size(); proc_id++)
         {
             if (sec_levels[proc_id] != sec_level)
@@ -230,9 +218,7 @@ std::vector<EncryptTask> Engine::maximizeSecurity(int64_t file_size, int64_t tim
             all_tasks[proc_id][block_id] = std::vector<Task>(devices.size());
             for (int device_id = 0; device_id < devices.size(); device_id++)
             {
-                std::stringstream ss;
-                ss << "_" << block_id << "_" << proc_id << "_" << device_id;
-                sat::BoolVar chosen = cp_model.NewBoolVar().WithName("chosen" + ss.str());
+                sat::BoolVar chosen = cp_model.NewBoolVar();
                 all_chosen.push_back(chosen);
                 all_block_sizes.push_back(blocks[block_id]);
                 all_sec_levels.push_back(sec_levels[proc_id]);
@@ -242,16 +228,16 @@ std::vector<EncryptTask> Engine::maximizeSecurity(int64_t file_size, int64_t tim
                 all_weighted_sec_levels.push_back((int64_t) std::pow(block_rank, sec_levels[proc_id]));
 
                 sat::IntVar p_time = cp_model.NewConstant(blocks[block_id] * processors[proc_id][block_id]);
-                sat::IntVar p_start = cp_model.NewIntVar(domain).WithName("p_start" + ss.str());
-                sat::IntVar p_end = cp_model.NewIntVar(domain).WithName("p_end" + ss.str());
-                sat::IntervalVar p_interval = cp_model.NewOptionalIntervalVar(p_start, p_time, p_end, chosen).WithName("p_interval" + ss.str());
+                sat::IntVar p_start = cp_model.NewIntVar(domain);
+                sat::IntVar p_end = cp_model.NewIntVar(domain);
+                sat::IntervalVar p_interval = cp_model.NewOptionalIntervalVar(p_start, p_time, p_end, chosen);
 
                 all_p_intervals.push_back(p_interval);
 
                 sat::IntVar io_time = cp_model.NewConstant(blocks[block_id] * devices[device_id]);
-                sat::IntVar io_start = cp_model.NewIntVar(domain).WithName("io_start" + ss.str());
-                sat::IntVar io_end = cp_model.NewIntVar(domain).WithName("io_end" + ss.str());
-                sat::IntervalVar io_interval = cp_model.NewOptionalIntervalVar(io_start, io_time, io_end, chosen).WithName("io_interval" + ss.str());
+                sat::IntVar io_start = cp_model.NewIntVar(domain);
+                sat::IntVar io_end = cp_model.NewIntVar(domain);
+                sat::IntervalVar io_interval = cp_model.NewOptionalIntervalVar(io_start, io_time, io_end, chosen);
 
                 all_io_ends.push_back(io_end);
                 per_device_intervals[device_id].push_back(io_interval);
@@ -265,9 +251,6 @@ std::vector<EncryptTask> Engine::maximizeSecurity(int64_t file_size, int64_t tim
         }
     }
 
-    std::cout << "Finished preparing data" << std::endl;
-    std::cout << "\tConsidering " << task_count << " tasks" << std::endl;
-
     /// Constraints
     cp_model.AddNoOverlap(all_p_intervals);
     for (int device_id = 0; device_id < devices.size(); device_id++)
@@ -277,22 +260,18 @@ std::vector<EncryptTask> Engine::maximizeSecurity(int64_t file_size, int64_t tim
 
     cp_model.AddEquality(sat::LinearExpr::BooleanScalProd(all_chosen, all_block_sizes), file_size);
 
-    sat::IntVar makespan = cp_model.NewIntVar(domain).WithName("makespan");
+    sat::IntVar makespan = cp_model.NewIntVar(domain);
     cp_model.AddMaxEquality(makespan, all_io_ends);
     cp_model.AddLessOrEqual(makespan, time_available * 1000000);
-
-    // Just for evaluation purposes
-    sat::IntVar overall_security = cp_model.NewIntVar(domain).WithName("overall_security");
-    cp_model.AddEquality(sat::LinearExpr::BooleanScalProd(all_chosen, all_sec_levels), overall_security);
 
     /// Objective
     cp_model.Maximize(makespan);
 
-    sat::IntVar weighted_security = cp_model.NewIntVar(domain).WithName("weighted_security");
+    sat::IntVar weighted_security = cp_model.NewIntVar(domain);
     cp_model.AddEquality(sat::LinearExpr::BooleanScalProd(all_chosen, all_weighted_sec_levels), weighted_security);
     cp_model.Maximize(weighted_security);
 
-    sat::IntVar choices = cp_model.NewIntVar({0, task_count}).WithName("choices");
+    sat::IntVar choices = cp_model.NewIntVar({0, task_count});
     cp_model.AddEquality(choices, sat::LinearExpr::BooleanSum(all_chosen));
     cp_model.Minimize(choices);
 
@@ -309,16 +288,10 @@ std::vector<EncryptTask> Engine::maximizeSecurity(int64_t file_size, int64_t tim
     std::vector<EncryptTask> result;
     if (response.status() == sat::CpSolverStatus::OPTIMAL || response.status() == sat::CpSolverStatus::FEASIBLE)
     {
-        auto mean_sec_level = (double) sat::SolutionIntegerValue(response, overall_security) / sat::SolutionIntegerValue(response, choices);
-
-        std::stringstream processor_tasks;
         for (int proc_id = 0; proc_id < processors.size(); proc_id++)
         {
             for (int device_id = 0; device_id < devices.size(); device_id++)
             {
-                std::stringstream chosen_blocks;
-                std::stringstream proc_times;
-                bool print = false;
                 for (int block_id = 0; block_id < blocks.size(); block_id++)
                 {
                     if (processors[proc_id][block_id] == 0 || blocks[block_id] > file_size)
