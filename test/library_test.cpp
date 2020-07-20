e683c7607bb6be537b41abff1b0fac28184109b4
Inaki Segura <isegura@hawk.iit.edu>
2020-07-20 12:32:28 +0200

    Removed library test code

--- test/library_test.cpp
+++ test/library_test.cpp
@@ -88,45 +88,4 @@ const CipherFactory* LibraryFixture::toFactory(const std::string &lib_name)
 
 TEST_F(LibraryFixture, MinTime)
 {
-    KeyManager key_manager(key_filename);
-
-    std::ifstream plaintext_file;
-    plaintext_file.open(plaintext_filename, std::ios::binary);
-    plaintext_size = obtainFileSize(plaintext_file);
-
-    Engine eng = Engine::loadEngine(system_profile_file_name, cipher_seed_file_name);
-    std::vector<EncryptTask> scheduling = eng.minimizeTime(30, plaintext_size, sec_level);
-
-    int64_t position = 0;
-    for (const EncryptTask &t : scheduling)
-    {
-        byte_ptr input_buffer = byte_ptr(new byte[t.block_len + 1024], std::default_delete<byte[]>());
-        byte_ptr output_buffer = byte_ptr(new byte[t.block_len + 1024], std::default_delete<byte[]>());
-
-        Cipher cipher = toCipher(t.alg_name, t.key_len, t.mode_name);
-        const CipherFactory *factory = toFactory(t.lib_name);
-
-        byte_len block_len = remainingFileLen(plaintext_size, position, t.block_len);
-        readInputFile(plaintext_file, input_buffer.get(), position, block_len);
-
-        CipherPtr cipher_ptr;
-        try
-        {
-            cipher_ptr = factory->getCipher(cipher);
-        } catch (UnsupportedCipherException &ex)
-        {
-            return;
-        }
-
-        const byte *key = key_manager.getKeyBySize(cipher_ptr->getKeyLen());
-        byte_len output_size = block_len + 1024;
-        cipher_ptr->encrypt(key, input_buffer.get(), block_len, output_buffer.get(), output_size);
-
-        writeOutputFile(t.device_name + "/" + plaintext_filename, output_buffer.get(), output_size);
-
-        position += t.block_len;
-    }
-
-    plaintext_file.sync();
-    plaintext_file.close();
 }
