e464614b17523b5cf0a51c99ee1665bd358edcfd
Inaki Segura <isegura@hawk.iit.edu>
2020-07-20 12:35:19 +0200

    Fixed file utilities declarations

--- include/CryptoBench/file_utilities.hpp
+++ include/CryptoBench/file_utilities.hpp
@@ -6,6 +6,8 @@
 #define CRYPTOBENCH_FILE_UTILITIES_HPP
 
 #include <fstream>
+#include <set>
+#include <vector>
 
 typedef unsigned char byte;
 typedef unsigned long long int byte_len;
@@ -14,10 +16,12 @@ byte_len remainingFileLen(byte_len plaintext_size, byte_len start_pos, byte_len 
 
 void readInputFile(std::ifstream &t, byte *input_text, byte_len start_pos, byte_len len);
 
-void writeOutputFile(const std::string& filename, byte *output_text, byte_len output_size);
+void writeOutputFile(std::ofstream &t, byte *output_text, byte_len output_size);
 
 byte_len obtainFileSize(std::ifstream &t);
 
 byte_len min(byte_len x, byte_len y);
+
+std::vector<std::string> splitPath(const std::string& str, const std::set<char> &delimiters);
 
 #endif //CRYPTOBENCH_FILE_UTILITIES_HPP
