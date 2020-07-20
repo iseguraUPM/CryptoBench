35412991da2ef6110c7f6f5f2909f68b7df4a5f8
Inaki Segura <isegura@hawk.iit.edu>
2020-07-20 12:32:15 +0200

    Fixed obtain file function to leave the pointer at the beginning. Added split path function

--- src/file_utilities.cpp
+++ src/file_utilities.cpp
@@ -4,6 +4,8 @@
 
 #include "CryptoBench/file_utilities.hpp"
 
+#include <vector>
+
 byte_len remainingFileLen(const byte_len plaintext_size, const byte_len start_pos, const byte_len block_size)
 {
     byte_len len = min(plaintext_size - start_pos, block_size);
@@ -19,27 +21,52 @@ void readInputFile(std::ifstream &t, byte *input_text, const byte_len start_pos,
     }
 }
 
-void writeOutputFile(const std::string& filename, byte *output_text, byte_len output_size)
+void writeOutputFile(std::ofstream &t, byte *output_text, byte_len output_size)
 {
-    std::ofstream output_file;
-    output_file.open(filename, std::ios::binary);
-
-    if (!output_file.write(reinterpret_cast<const char *>(output_text), output_size))
+    if (!t.write(reinterpret_cast<const char *>(output_text), output_size))
     {
         throw std::runtime_error("Error writing " + std::to_string(output_size) + "B file");
     }
-
-    output_file.flush();
-    output_file.close();
 }
 
 byte_len obtainFileSize(std::ifstream &t)
 {
     t.seekg(0, std::ios::end);
-    return t.tellg();
+    byte_len size = t.tellg();
+    t.seekg(0, std::ios::beg);
+    return size;
 }
 
 byte_len min(byte_len x, byte_len y)
 {
     return x > y ? y : x;
+}
+
+std::vector<std::string> splitPath(
+        const std::string& str
+        , const std::set<char> &delimiters)
+{
+    std::vector<std::string> result;
+
+    char const* pch = str.c_str();
+    char const* start = pch;
+    for(; *pch; ++pch)
+    {
+        if (delimiters.find(*pch) != delimiters.end())
+        {
+            if (start != pch)
+            {
+                std::string str(start, pch);
+                result.push_back(str);
+            }
+            else
+            {
+                result.push_back("");
+            }
+            start = pch + 1;
+        }
+    }
+    result.push_back(start);
+
+    return result;
 }
